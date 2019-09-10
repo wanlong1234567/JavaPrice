459226b8c45e5e92cebb54825664e0da5b048aa4
wanlong <wanlong3@jd.com>
2019-09-10 21:10:35 +0800

    Java基础项目

---
+++ JavaBasic/src/main/java/com/wanlong/demo1.java
@@ -0,0 +1,13 @@
+package com.wanlong;
+
+/**
+ * @author wanlong
+ * @date 2019/9/10
+ */
+public class demo1 {
+    public static void main(String[] args) {
+        String data = "hello world";
+        char[] arr = data.toCharArray();
+        System.out.println("arr:"+arr.length);
+    }
+}
